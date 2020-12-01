c26018e099d1e91e5bed83b36cdc7c8a8921b6c4
Michael Thiem <mthiem0@gmail.com>
2020-12-01 07:24:51 +0000

    test manual assume role

--- scripts/assume-role.sh
+++ scripts/assume-role.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-CFN_STACK_NAME=$1
+ACCOUNT_ID=$1
+ROLE_NAME=$2
 
-EKS_ADMIN_ROLE_ARN=$(aws cloudformation describe-stacks --stack-name $CFN_STACK_NAME --region eu-central-1 --query "Stacks[0].Outputs[?OutputKey=='EKSAdminRoleARN'].OutputValue" --output text)
+EKS_ADMIN_ROLE_ARN="arn:aws:iam::$ACCOUNT_ID:role/$ROLE_NAME"
 
 ASSUME_ROLE_OUTPUT=$(aws sts assume-role --role-arn $EKS_ADMIN_ROLE_ARN --role-session-name eks-setup-session)
 ASSUME_ROLE_ENVIRONMENT=$(echo $ASSUME_ROLE_OUTPUT | jq -r '.Credentials | .["AWS_ACCESS_KEY_ID"] = .AccessKeyId | .["AWS_SECRET_ACCESS_KEY"] = .SecretAccessKey | .["AWS_SECURITY_TOKEN"] = .SessionToken | del(.AccessKeyId, .SecretAccessKey, .SessionToken, .Expiration) | to_entries[] | "export \(.key)=\(.value)"')
