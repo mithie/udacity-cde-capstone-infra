7ef705031ee82f522eb6cbcd549aa949ca27a9e1
Michael Thiem <mthiem0@gmail.com>
2020-12-01 09:15:47 +0000

    test manual assume role

--- scripts/prepare-iam.sh
+++ scripts/prepare-iam.sh
@@ -44,3 +44,6 @@ eval $ACCESS_KEY_ENVIRONMENT
 # ~/.aws/credentials [default]
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
